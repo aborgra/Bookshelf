e306b816749e27e438e803ffb1974030fcf47156
aborgra <audrey.borgra@gmail.com>
2020-04-23 15:49:48 -0500

    full CRUD

---
+++ Bookshelf/Data/ApplicationDbContext.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Bookshelf.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bookshelf.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<Book> Book { get; set; }
+        public DbSet<Genre> Genre { get; set; }
+
+        public DbSet<BookGenre> BookGenre { get; set; }
+
+        protected override void  OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Genre>().HasData(new Genre()
+            {
+                Id = 1,
+                Name = "Fiction"
+            });
+            modelBuilder.Entity<Genre>().HasData(new Genre()
+            {
+                Id = 2,
+                Name = "Nonfiction"
+            });
+            modelBuilder.Entity<Genre>().HasData(new Genre()
+            {
+                Id = 3,
+                Name = "Science Fiction"
+            });
+            modelBuilder.Entity<Genre>().HasData(new Genre()
+            {
+                Id = 4,
+                Name = "Romance"
+            });
+            modelBuilder.Entity<Genre>().HasData(new Genre()
+            {
+                Id = 5,
+                Name = "Mystery"
+            });
+
+
+            //If you name your foreign keys correctly, then you don't need this.
+            modelBuilder.Entity<BookGenre>()
+                .HasOne(pt => pt.Book)
+                .WithMany(p => p.BookGenres)
+                .HasForeignKey(pt => pt.BookId);
+
+            modelBuilder.Entity<BookGenre>()
+                .HasOne(pt => pt.Genre)
+                .WithMany(t => t.BookGenres)
+                .HasForeignKey(pt => pt.GenreId);
+        }
+
+    }
+}
---
+++ Bookshelf/Models/BookFormViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookshelf.Models
+{
+    public class BookFormViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public List<SelectListItem> GenreOptions { get; set; }
+        [NotMapped]
+        public List<int> SelectedGenreIds { get; set; }
+    }
+}
